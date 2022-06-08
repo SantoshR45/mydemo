4970890e83c87db1ea43e24b70d2732d6f895362
santoshronanki <ronanki.santosh@gmail.com>
2022-06-08 09:34:33 -0600

    done

--- App.py
+++ App.py
@@ -1,19 +1,11 @@
-s= "welcome to my world"
-print(s.capitalize())
-print(s.center(40,"*"))
-print(s.count("o",7,len(s)))
-print(s.endswith("yfg"))
-print(s.endswith("orld"))
-print(s.startswith("wel"))
-print(s.rfind("e"))
-print(s.find("e"))
-print(s.index("m"))
-print(s.isalnum())
-print(s.isalpha())
-print(s.rjust(30,'*'))
-print(s.upper())
-print(s.lower())
-print(s.replace("world","company"))
-print(s.split(" "))
-print(s.swapcase())
-print(s.zfill(40))
+a=int(input("enter your credit score - "))
+price = 100000
+if a >= 790:
+    print("Your credit score is excellent and your down payment is $", 0.14*price)
+elif a < 790 and a >= 710:
+    print("your credit score is good so your down payment is $", 0.29 * price)
+elif a<710 and a>=650:
+    print("your credit score is average so your down payment is $", 0.36*price)
+else:
+    print("As your credit score is poor, you are not eligible for this offer but if you are still interested to buy then your minimum down payment is $", 0.65*price)
+
