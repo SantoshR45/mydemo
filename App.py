6689bd166b50852a2d7bef15c88a88dc50db734f
SantoshR45 <105248424+SantoshR45@users.noreply.github.com>
2022-06-08 11:24:12 -0400

    credit check update

---
+++ App.py
@@ -0,0 +1,11 @@
+a=int(input("enter your credit score - "))
+price = 100000
+if a >= 790:
+    print("Your credit score is excellent and your down payment is $", 0.14*price)
+elif a < 790 and a >= 710:
+    print("your credit score is good so your down payment is $", 0.26 * price)
+elif a<710 and a>=650:
+    print("your credit score is average so your down payment is $", 0.36*price)
+else:
+    print("As your credit score is poor, you are not eligible for this offer but if you are still interested to buy then your minimum down payment is $", 0.65*price)
+
